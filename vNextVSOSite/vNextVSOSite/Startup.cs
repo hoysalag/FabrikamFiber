033da12e2b2a6e3e4325e863edfa5fe14e965a48
Hoysala <hoysala@users.noreply.github.com>
2015-06-19 07:18:33 +0000

    Added new Site

---
+++ vNextVSOSite/vNextVSOSite/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(vNextVSOSite.Startup))]
+namespace vNextVSOSite
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
